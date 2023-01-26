60b45905f860e231fa0314ca83305d64609a2618
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-26 20:44:32 +0000

    Created new Contact webpage and assigned path to Lini

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+const Contact = () => {
+    return(
+        <>
+            <h2>Contact</h2>
+            <p>Contact the creator at:</p>
+            <ul>
+                <li>Email: me@me.com</li>
+                <li>Phone: +44 11345293292</li>
+            </ul>
+        
+        </>
+        
+    )
+}
+
+export default Contact;
--- src/containers/QuoteContainer.js
+++ src/containers/QuoteContainer.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import About from '../components/About';
+import Contact from '../components/Contact';
 import Quote from '../components/Quote';
 import QuoteList from '../components/QuoteList';
 
@@ -25,7 +26,7 @@ const QuoteContainer = () =>{
                 <ul>
                     <li><Link to ="/">Home</Link></li>
                     <li><Link to = "/about">About</Link></li>
-                    <li><Link to ="/contacts">Contacts</Link></li>
+                    <li><Link to ="/contact">Contact</Link></li>
                 </ul>
 
                 <button>
@@ -36,7 +37,7 @@ const QuoteContainer = () =>{
 
             <Routes>
                 <Route path = "/about" element = {<About/>}/>
-                <Route></Route>
+                <Route path = "/contact" element = {<Contact/>}/>
             </Routes>   
         
         </BrowserRouter>    
