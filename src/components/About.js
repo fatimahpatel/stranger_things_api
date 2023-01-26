b43b6f9b049e6bb5a8c9894a659fa521977a9586
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-26 20:38:13 +0000

    Added Home, About and Contacts link with Quotes button

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <>
+            <h2>About</h2>
+            <p>This page tells us what the website is about</p>
+        </>
+        
+    )
+}
+
+export default About;
--- src/containers/QuoteContainer.js
+++ src/containers/QuoteContainer.js
@@ -1,6 +1,10 @@
 import {useState, useEffect} from 'react'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import About from '../components/About';
 import Quote from '../components/Quote';
 import QuoteList from '../components/QuoteList';
+
+
 const QuoteContainer = () =>{
     //need to fetch from api and return quote
     const [quotesFromApi, setQuotesFromApi] = useState([]);
@@ -16,7 +20,26 @@ const QuoteContainer = () =>{
 
  
     return(
-        <QuoteList quotes = {quotesFromApi}/>
+        <BrowserRouter>
+            <h1>Stranger Things Quotes</h1>
+                <ul>
+                    <li><Link to ="/">Home</Link></li>
+                    <li><Link to = "/about">About</Link></li>
+                    <li><Link to ="/contacts">Contacts</Link></li>
+                </ul>
+
+                <button>
+                    <Link to ="/quotes">Quotes</Link>
+                </button>
+
+
+
+            <Routes>
+                <Route path = "/about" element = {<About/>}/>
+                <Route></Route>
+            </Routes>   
+        
+        </BrowserRouter>    
     )
 }
 export default QuoteContainer;
