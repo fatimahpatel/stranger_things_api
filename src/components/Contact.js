5de682ea1b259e69e94c871504ad34fa6ac11d5b
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-30 15:42:29 +0000

    updated code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,11 @@ const Contact = () => {
                 <li>Phone: +44 11345293292</li>
             </ul>
 
-            <img src = "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/10/27/11/stranger-things-mike.jpg?width=968&auto=webp&quality=50&crop=968%3A645%2Csmart" alt = "mke wheeler from stranger things holding phone"/>
+            <img src = "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/10/27/11/stranger-things-mike.jpg?width=968&auto=webp&quality=50&crop=968%3A645%2Csmart" alt = "mike wheeler from stranger things holding radio"/>
+
+
+            <img src = "https://images.thestar.com/gg5Ja3D8arejdncWN_-7cCKBdrE=/1086x725/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/entertainment/television/2016/08/05/netflix-dives-into-virtual-reality-with-stranger-things/winona-ryder.jpg" alt = "Joyce Byers from stranger things holding phone to ear"/>
+        
         
         </>
         
